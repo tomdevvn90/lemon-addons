4c7480d72c0dcbc3245bbae55b108744fe8221ac
Matt <matt.beplus@gmail.com>
2024-01-02 14:40:35 +0700

    update skin grid beauty posts carousel

--- widgets/posts-carousel/skins/skin-grid-beauty.php
+++ widgets/posts-carousel/skins/skin-grid-beauty.php
@@ -608,6 +608,7 @@ class Skin_Grid_Beauty extends Skin_Base {
 				'default' => '',
 				'selectors' => [
 					'{{WRAPPER}} .elementor-post__meta .meta-author a' => 'color: {{VALUE}};',
+                    '{{WRAPPER}} .elementor-post__meta .meta-author a svg' => 'fill: {{VALUE}};',
 				],
 				'condition' => [
 					'skin_grid_beauty_show_meta!' => '',
@@ -653,7 +654,8 @@ class Skin_Grid_Beauty extends Skin_Base {
                         <li class="meta-author">
                             <?php
                                 echo '<a class="author" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">'.
-                                    '<span>' . esc_html__('Posted By ', 'bearsthemes-addons') . '</span>' . get_the_author() .
+                                    '<svg aria-hidden="true" class="e-font-icon-svg e-fas-user" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
+                                    <span>' . esc_html__('Posted By ', 'bearsthemes-addons') . '</span>' . get_the_author() .
                                 '</a>';
                             ?>
                         </li>
